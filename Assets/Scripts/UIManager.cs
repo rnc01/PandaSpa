3e0aa090bd55ca53f15a41ac5ede6b8ac1deaa2a
hyunmin0 <khmin0593@gmail.com>
2023-09-04 14:51:04 +0900

    Coin Save

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
+
 
 
 public class UIManager : MonoBehaviour
@@ -14,7 +16,7 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI text;
 
-    private int coin = 0;
+    public int coin = 0;
 
     void Awake()
     {
@@ -24,10 +26,32 @@ public class UIManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+
+    }
+
+    // 돈이 증가하여 텍스트로 출력하는 함수
     public void IncreaseCoin(int increase)
     {
         coin += 1 * increase;
         text.SetText(coin.ToString());
+    }
+
+    // 게임 요소 저장하는 함수
+    public void GameSave()
+    {
+        PlayerPrefs.SetInt("Coin", coin);
+        //PlayerPrefs.SetFloat("PandaX", baby_panda_prototype.transform.position.x);
+        //PlayerPrefs.SetFloat("PandaY", baby_panda_prototype.transform.position.y);
+        PlayerPrefs.Save();
+    }
+
+    public void GameLoad()
+    {
+        int sCoin = PlayerPrefs.GetInt("Coin");
+
+        coin = sCoin;
     }
 
     public void CreatePandaPrototype(GameObject pandaPrototype)
