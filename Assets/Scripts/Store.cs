032e42b3d0bd453414ef9ec84ef47b276864bfe8
hyunmin0 <khmin0593@gmail.com>
2023-10-29 23:06:19 +0900

    상점버튼 상하수정

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -16,12 +16,12 @@ public class Store : MonoBehaviour
         if(StorePanel.transform.position.y > 130)
         {
             StorePanel.transform.position += new Vector3(0, -290f);
-            transform.localScale = new Vector3(1,-1,1);
+            transform.localScale = new Vector3(1,1,1);
         }
         else if (StorePanel.transform.position.y < -130)
         {
             StorePanel.transform.position += new Vector3(0, 290f);
-            transform.localScale = new Vector3(1, 1, 1);
+            transform.localScale = new Vector3(1,-1, 1);
         }
         // 튜토리얼 첫번째 숨기기, 두번째 보이기
         if (TStpe1Panel.activeSelf == true)
